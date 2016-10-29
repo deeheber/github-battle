7ba1554a0d69bb2e9f4537880b526be772ac6732
Danielle H <daheber@gmail.com>
2016-10-28 18:24:57 -0700

    home, playerOne, and playerTwo routing/components

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,20 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = require('react-router').Link;
 
 var Home = React.createClass({
     render: function(){
         return (
-            <div>
-                Hello from home!
+            <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
+                <h1>GitHub Battle</h1>
+                <p className='lead'>Some fancy motto</p>
+                <Link to='/playerOne'>
+                    <button type='button' className='btn btn-large btn-success'>
+                        Get Started
+                    </button>
+
+                </Link>
             </div>
         )
     }
