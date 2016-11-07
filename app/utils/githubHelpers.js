8575a865e09f5fd32b41fe17ece1245ef78b7dea
Danielle H <daheber@gmail.com>
2016-11-07 14:17:45 -0800

    fat arrows in githubHelpers

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -14,7 +14,7 @@ function getRepos(username){
 }
 
 function getTotalStars(repos){
-    return repos.data.reduce(function(prev, current){
+    return repos.data.reduce((prev, current)=>{
         return prev + current.stargazers_count;
     }, 0);
 }
@@ -22,7 +22,7 @@ function getTotalStars(repos){
 function getPlayersData(player){
     return getRepos(player.login)
         .then(getTotalStars)
-        .then(function(totalStars){
+        .then(totalStars=>{
             return {
                 followers: player.followers,
                 totalStars: totalStars
@@ -39,15 +39,15 @@ function calculateScores(players){
 
 const helpers = {
     getPlayersInfo: function(players){
-        return axios.all(players.map(function(username){
+        return axios.all(players.map(username=>{
             return getUserInfo(username);
         }))
             .then(function(info){
-                return info.map(function(user){
+                return info.map(user=>{
                     return user.data;
                 })
             })
-            .catch(function(error){
+            .catch(error=>{
                 return logCustomMessage(error.statusText, {
                     players: players,
                     error: error
@@ -60,7 +60,7 @@ const helpers = {
 
         return axios.all([playerOneData, playerTwoData])
             .then(calculateScores)
-            .catch(function(error){
+            .catch(error=>{
                 return logCustomMessage(error.statusText, {
                     players: players,
                     error: error
