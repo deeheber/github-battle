de7ad57dd8522a5a407d65a1b06a59190e9e7f4c
Danielle H <daheber@gmail.com>
2016-12-18 11:07:37 -0800

    Prompt conversion to es6

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -1,51 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Prompt from '../components/Prompt';
 
-var PromptContainer = React.createClass({
-    contextTypes: {
-        router: React.PropTypes.object.isRequired
-    },
-    getInitialState: function(){
-        return {
-            username: ''
-        }
-            
-    },
-    handleUpdateUser: function(e){
-        this.setState({
-            username: e.target.value
-        })
-    },
-    handleSubmitUser: function(e){
-        e.preventDefault();
-        const username = this.state.username;
-        this.setState({
-            username: ''
-        })
-
-        if(this.props.routeParams.playerOne){
-            this.context.router.push({
-                pathname: '/battle',
-                query: {
-                    playerOne: this.props.routeParams.playerOne,
-                    playerTwo: this.state.username
-                }
-            })
-        } else {
-            this.context.router.push('/playerTwo/' + this.state.username);
-        }
-    },
-    render: function(){
-        return (
-            <Prompt 
-                onSubmitUser={this.handleSubmitUser}
-                onUpdateUser={this.handleUpdateUser}
-                header={this.props.route.header}
-                username={this.state.username}
-            />
-        )
+class PromptContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      username: ''
     }
-});
+  }
+  handleUpdateUser(e){
+      this.setState({
+          username: e.target.value
+      });
+  }
+  handleSubmitUser(e){
+      e.preventDefault();
+      const username = this.state.username;
+      this.setState({
+          username: ''
+      });
+
+      if(this.props.routeParams.playerOne){
+          this.context.router.push({
+              pathname: '/battle',
+              query: {
+                  playerOne: this.props.routeParams.playerOne,
+                  playerTwo: this.state.username
+              }
+          })
+      } else {
+          this.context.router.push('/playerTwo/' + this.state.username);
+      }
+  }
+  render(){
+    return (
+      <Prompt 
+        onSubmitUser={e=>{this.handleSubmitUser(e)}}
+        onUpdateUser={e=>{this.handleUpdateUser(e)}}
+        header={this.props.route.header}
+        username={this.state.username}
+      />
+    )
+  }
+}
+
+PromptContainer.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default PromptContainer;
 
