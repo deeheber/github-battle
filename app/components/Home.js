0895c2c72017fc39dfcd921e3f64055aeb830a33
Danielle H <daheber@gmail.com>
2016-12-18 14:06:23 -0800

    Results conversion to es6

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,19 +3,19 @@ import ReactRouter, { Link } from 'react-router';
 import MainContainer from './MainContainer';
 
 class Home extends Component {
-    render(){
-      return (
-        <MainContainer>
-          <h1>GitHub Battle</h1>
-          <p className='lead'>Who will be victorious?</p>
-          <Link to='/playerOne'>
-            <button type='button' className='btn btn-large btn-success'>
-                Get Started
-            </button>
-          </Link>
-        </MainContainer>
-      )
-    }
+  render(){
+  return (
+    <MainContainer>
+      <h1>GitHub Battle</h1>
+      <p className='lead'>Who will be victorious?</p>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-large btn-success'>
+          Get Started
+        </button>
+      </Link>
+    </MainContainer>
+  )
+  }
 }
 
 export default Home;
