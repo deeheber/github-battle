00a2a4b7f39f0eb512da95b5f3b6a38dca8e0ac3
Danielle H <daheber@gmail.com>
2016-12-18 14:01:19 -0800

    added fat arrows and other es6 updates

--- app/utils/githubHelpers.js
+++ app/utils/githubHelpers.js
@@ -6,66 +6,66 @@ const sec = process.env.SECRET;
 const param = '?client_id=' + id + '&client_secret=' + sec;
 
 function getUserInfo(username){
-    return axios.get('https://api.github.com/users/' + username + param);
+  return axios.get('https://api.github.com/users/' + username + param);
 }
 
 function getRepos(username){
-    return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
+  return axios.get('https://api.github.com/users/' + username + '/repos' + param + '&per_page=100');
 }
 
 function getTotalStars(repos){
-    return repos.data.reduce((prev, current)=>{
-        return prev + current.stargazers_count;
-    }, 0);
+  return repos.data.reduce((prev, current)=>{
+    return prev + current.stargazers_count;
+  }, 0);
 }
 
 function getPlayersData(player){
-    return getRepos(player.login)
-        .then(getTotalStars)
-        .then(totalStars=>{
-            return {
-                followers: player.followers,
-                totalStars: totalStars
-            }
-        })
+  return getRepos(player.login)
+    .then(getTotalStars)
+    .then(totalStars=>{
+      return {
+        followers: player.followers,
+        totalStars: totalStars
+      }
+    })
 }
 
 function calculateScores(players){
-    return [
-        players[0].followers * 3 + players[0].totalStars,
-        players[1].followers * 3 + players[1].totalStars
-    ]
+  return [
+    players[0].followers * 3 + players[0].totalStars,
+    players[1].followers * 3 + players[1].totalStars
+  ]
 }
 
 const helpers = {
-    getPlayersInfo: function(players){
+    getPlayersInfo: players=>{
         return axios.all(players.map(username=>{
-            return getUserInfo(username);
+          return getUserInfo(username);
         }))
-            .then(function(info){
-                return info.map(user=>{
-                    return user.data;
-                })
-            })
-            .catch(error=>{
-                return logCustomMessage(error.statusText, {
-                    players: players,
-                    error: error
-                })
-            });
+        .then(info=>{
+          return info.map(user=>{
+            return user.data;
+          })
+        })
+        .catch(error=>{
+          return logCustomMessage(error.statusText, {
+            players: players,
+            error: error
+          })
+        });
     },
-    battle: function(players){
-        const playerOneData = getPlayersData(players[0])
-        const playerTwoData = getPlayersData(players[1]);
+    battle: players=>{
+      const playerOneData = getPlayersData(players[0])
+      const playerTwoData = getPlayersData(players[1]);
 
-        return axios.all([playerOneData, playerTwoData])
-            .then(calculateScores)
-            .catch(error=>{
-                return logCustomMessage(error.statusText, {
-                    players: players,
-                    error: error
-                })
-            })
+      return axios.all([playerOneData, playerTwoData])
+        .then(calculateScores)
+        .catch(error=>{
+          return logCustomMessage(error.statusText, {
+            players: players,
+            error: error
+          })
+        });
     }
 };
 
