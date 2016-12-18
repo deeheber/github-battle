2fc512d714cd768a626342a9aa8dc909288fb9f2
Danielle H <daheber@gmail.com>
2016-12-18 11:08:56 -0800

    indention fix

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -9,28 +9,28 @@ class PromptContainer extends Component {
     }
   }
   handleUpdateUser(e){
-      this.setState({
-          username: e.target.value
-      });
+    this.setState({
+        username: e.target.value
+    });
   }
   handleSubmitUser(e){
-      e.preventDefault();
-      const username = this.state.username;
-      this.setState({
-          username: ''
-      });
+    e.preventDefault();
+    const username = this.state.username;
+    this.setState({
+      username: ''
+    });
 
-      if(this.props.routeParams.playerOne){
-          this.context.router.push({
-              pathname: '/battle',
-              query: {
-                  playerOne: this.props.routeParams.playerOne,
-                  playerTwo: this.state.username
-              }
-          })
-      } else {
-          this.context.router.push('/playerTwo/' + this.state.username);
-      }
+    if(this.props.routeParams.playerOne){
+      this.context.router.push({
+        pathname: '/battle',
+        query: {
+          playerOne: this.props.routeParams.playerOne,
+          playerTwo: this.state.username
+        }
+      });
+    } else {
+      this.context.router.push('/playerTwo/' + this.state.username);
+    }
   }
   render(){
     return (
